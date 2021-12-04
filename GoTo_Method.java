f7cb8f4bd44579fd6f6f801f4581902641d4e286
Mustafa Tortuk <mustafat237@hotmail.com>
2021-12-04 23:45:54 +0300

    using goto with boolean

---
+++ GoTo_Method.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class GoTo_Method
+{
+	public static int total(int a)
+	{
+		return a*(a+1)/2;
+	}
+	
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		boolean control= true; //our variables for goto*
+		int n = 1;
+		while(control != false)
+		{
+			
+		    System.out.print("Enter a random number between 1 and 100: ");
+		    n = input.nextInt();
+			if(n>=1 && n<=100)
+			{
+				control = false;
+			}
+			
+			else{
+				control = true;
+			}
+		}
+		
+		
+		System.out.println("sum of number from 1 to "+n+" : "+total(n));
+		
+		input.close();
+	}
+}
