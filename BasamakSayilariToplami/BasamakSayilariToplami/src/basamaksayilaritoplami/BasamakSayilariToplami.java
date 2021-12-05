aab64c77636141cb59a1f54b7239a6e414a5cf9b
Mustafa Tortuk <mustafat237@hotmail.com>
2021-12-06 01:54:11 +0300

    r

---
+++ BasamakSayilariToplami/BasamakSayilariToplami/src/basamaksayilaritoplami/BasamakSayilariToplami.java
@@ -0,0 +1,23 @@
+package basamaksayilaritoplami;
+import java.util.Scanner;
+
+public class BasamakSayilariToplami {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a random number :");
+        int number = input.nextInt();
+        int digit = 0;//Basamak sayisini tutan değisken
+        
+        int total = 0;//Basamak sayilarinin toplamini tutan değisken
+        
+        while(number>0)
+        {
+            digit = number %10;
+            total = total+digit;
+            number = number/10;
+        }
+        
+        System.out.println("sum of digits : "+total);
+    }
+    
+}
