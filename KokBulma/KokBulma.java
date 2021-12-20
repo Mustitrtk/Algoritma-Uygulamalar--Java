ca76e44d3eab471a91a2e23d2fc90361575e3183
Mustafa Tortuk <mustafat237@hotmail.com>
2021-12-21 01:46:44 +0300

    Add files via upload

---
+++ KokBulma/KokBulma.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class KokBulma
+{
+	public static void kokIslem(int a, int b, int c)
+	{
+		int delta = b*b-4*(a*c);
+		
+		if(delta > 0 )
+		{
+			System.out.println("Polinomda 2 farkli kök vardir.");
+			System.out.println("kokler toplami :"+(-b/a)+"\n"+"Kökler carpimi :"+(c/a));
+		}
+		else if(delta == 0)
+		{
+			System.out.println("Polinomda 1 kök vardir.");
+			System.out.println("kokler toplami :"+(-b/a)+"\n"+"Kökler carpimi :"+(c/a));
+		}
+		else
+		{
+			System.out.println("Sistemde kök yoktur.");
+		}
+	}
+	
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		System.out.print("Baş katsayilari giriniz :");
+		int n = input.nextInt();
+		int n1 = input.nextInt(); 
+		int n2 = input.nextInt();
+		kokIslem(n,n1,n2);
+
+		input.close();
+	}
+}
